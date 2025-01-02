99da1b7ce3d95b4f74b1a2a8bf7a73fc26cb7e76
Juri <juri.furer@pm.me>
2025-01-03 00:54:14 +0100

    enable nvidia for xserver

--- hosts/vanguard/configuration.nix
+++ hosts/vanguard/configuration.nix
@@ -55,7 +55,10 @@
 
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
+  services.xserver = {
+    enable = true;
+    videoDrivers = ["nvidia"];
+  };
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
