95b568a9e4d6c913a80871794c4c3ef582addb0b
Juri Furer <juri.furer@pm.me>
2026-09-22 09:56:16 +0200

    feat: add networking to the VMs

--- modules/nixos/virtualisation.nix
+++ modules/nixos/virtualisation.nix
@@ -1,7 +1,15 @@
 { ... }: {
-  flake.nixosModules.virtualisation = { ... }: {
+  flake.nixosModules.virtualisation = { pkgs, ... }: {
     # Enable QEMU/ KVM VMs and install Virt Manager
     virtualisation.libvirtd.enable = true;
     programs.virt-manager.enable = true;
+
+    customGroups.extraGroups = [ "libvirtd" ];
+
+    # Required for default NAT network DNS and DHCP functionality
+    environment.systemPackages = with pkgs; [ dnsmasq ];
+
+    # Allow the virtual bridge interface through the firewall
+    networking.firewall.trustedInterfaces = [ "virbr0" ];
   };
 }
